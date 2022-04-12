d886cb33e308bd62727150d03b16b0b16b8d5325
DerzhiZhabu <94835516+DerzhiZhabu@users.noreply.github.com>
2022-04-12 10:22:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ login_manager.init_app(app)
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 @login_manager.user_loader
